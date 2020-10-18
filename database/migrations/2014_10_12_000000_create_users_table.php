d43264b68f37eef3d263f24228b51389122b5a48
workwithAarohan <aarohan.nakarmi.29@gmail.com>
2020-10-18 12:14:26 +0545

    Category table create gareko

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('image');
+            $table->boolean('isAdmin')->default(0);
+            $table->string('address');
+            $table->string('contact');
+
             $table->rememberToken();
             $table->timestamps();
         });
